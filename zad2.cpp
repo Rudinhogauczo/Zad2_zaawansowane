fa92bbe5afc234a751baa06235aff211bd18538c
Rudinhogauczo <szymonpulit@gmail.com>
2023-11-03 17:04:03 +0100

    Usun_element_znajdz_najmniejszy

--- zad2.cpp
+++ zad2.cpp
@@ -28,4 +28,42 @@ Wezel* korzen;
         return wezel;
     }
 
+ // Usuwa element o podanym kluczu z drzewa BST.
+    void UsunElement(int klucz) {
+        korzen = UsunRekurencyjnie(korzen, klucz);
+    }
+ 
+    // Funkcja pomocnicza dla UsunElement do usuwania rekurencyjnie.
+    Wezel* UsunRekurencyjnie(Wezel* wezel, int klucz) {
+        if (wezel == nullptr) {
+            return wezel;
+        }
+        if (klucz < wezel->klucz) {
+            wezel->lewy = UsunRekurencyjnie(wezel->lewy, klucz);
+        } else if (klucz > wezel->klucz) {
+            wezel->prawy = UsunRekurencyjnie(wezel->prawy, klucz);
+        } else {
+            if (wezel->lewy == nullptr) {
+                Wezel* temp = wezel->prawy;
+                delete wezel;
+                return temp;
+            } else if (wezel->prawy == nullptr) {
+                Wezel* temp = wezel->lewy;
+                delete wezel;
+                return temp;
+            }
+            Wezel* temp = NajmniejszyWezel(wezel->prawy);
+            wezel->klucz = temp->klucz;
+            wezel->prawy = UsunRekurencyjnie(wezel->prawy, temp->klucz);
+        }
+        return wezel;
+    }
+// Znajduje najmniejszy element w drzewie.
+    Wezel* NajmniejszyWezel(Wezel* wezel) {
+        Wezel* aktualny = wezel;
+        while (aktualny->lewy != nullptr) {
+            aktualny = aktualny->lewy;
+        }
+        return aktualny;
+    }
 };
