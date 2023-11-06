07ac861ba23cdf9ae429c7abaed958511965a144
Klaudiusz02 <147625464+Klaudiusz02@users.noreply.github.com>
2023-11-06 21:04:52 +0100

    Dopisanie brakujących opcji w funkcji main

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ int main() {
         std::cout << "Menu Drzewa BST:" << std::endl;
         std::cout << "1. Dodaj element" << std::endl;
         std::cout << "2. Usun element" << std::endl;
+        std::cout << "3. Usun calkowicie drzewo" << std::endl;
+        std::cout << "4. Szukaj drogi do elementu" << std::endl;
+        std::cout << "5. Wyswietl drzewo" << std::endl;
         std::cout << "0. Wyjscie" << std::endl;
         std::cout << "Wybierz opcje: ";
         std::cin >> opcja;
@@ -27,6 +30,21 @@ int main() {
                 std::cout << "Podaj klucz elementu do usuniecia: ";
                 std::cin >> klucz;
                 drzewo.UsunElement(klucz);
+                break;
+            case 3:
+                drzewo.UsunCaleDrzewo(drzewo.PobierzKorzen());
+                drzewo.UstawKorzen(nullptr);
+                std::cout << "Drzewo zostalo usuniete." << std::endl;
+                break;
+            case 4:
+                std::cout << "Podaj klucz elementu do znalezienia: ";
+                std::cin >> klucz;
+                drzewo.SzukajDrogi(klucz);
+                break;
+            case 5:
+                std::cout << "Drzewo BST: ";
+                drzewo.WyswietlDrzewo(drzewo.PobierzKorzen());
+                std::cout << std::endl;
                 break;
             case 0:
                 std::cout << "Wyjscie z programu." << std::endl;
